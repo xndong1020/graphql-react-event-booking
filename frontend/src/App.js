4c89251f52806f79e67a5110846fd4b60e01c13b
Jeremy Gu <isdance2004@hotmail.com>
2019-02-28 18:17:47 +1100

    update css to scss

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import DefaultLayout from './layouts/DefaultLayout'
-import './App.css'
+import './App.scss'
 
 class App extends Component {
   render() {
--- frontend/src/components/navigation/MainNavigation.jsx
+++ frontend/src/components/navigation/MainNavigation.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-import './MainNavigation.css'
+import './MainNavigation.scss'
 
 const MainNavigation = () => {
   return (
